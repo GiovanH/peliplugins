2a8c63486bde7ccf894900f1615d2a37a6a62d2d
Gio <sethg@ipi.org>
2020-07-17 00:49:14 -0500

    It works on drafts

--- renderdeps.py
+++ renderdeps.py
@@ -3,13 +3,15 @@
 import logging
 from bs4 import BeautifulSoup
 from pelican import signals
-
+from pelican.generators import ArticlesGenerator, PagesGenerator
 
 def process_content(article):
     """
     Pelican callback
     """
+
     if article._content is None:
+        logging.warning(f"{article.title} is empty!")
         return
 
     settings = article.settings
@@ -17,10 +19,9 @@ def process_content(article):
     soup = BeautifulSoup(article._content, 'html.parser')
 
     dirty = False
-
+    
     for (args, kwargs), dep in dependencies:
-        ident = f"{args} {kwargs}"
-        logging.debug(f"Checking for '{ident}'")
+        logging.debug(f"Checking for '{args} {kwargs}'")
         if soup.find(*args, **kwargs):
             soup.append(BeautifulSoup(dep, 'html.parser'))
             dirty = True
@@ -31,13 +32,16 @@ def process_content(article):
     if dirty:
         article._content = soup.decode()
 
+    return
+
 
 def add_deps(generators):
-    from pelican.generators import ArticlesGenerator, PagesGenerator
     # Process the articles and pages
     for generator in generators:
         if isinstance(generator, ArticlesGenerator):
             for article in generator.articles:
+                process_content(article)
+            for article in generator.drafts:
                 process_content(article)
         elif isinstance(generator, PagesGenerator):
             for page in generator.pages:
