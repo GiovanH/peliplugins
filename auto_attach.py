c44aa5bced841cf3738b76c7525467eff46a7b65
Gio <sethg@ipi.org>
2020-08-28 21:37:36 -0500

    Updated autoattach to work on links

--- auto_attach.py
+++ auto_attach.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from markdown.extensions import Extension
 # import markdown.inlinepatterns as inlinepatterns
-# from markdown.inlinepatterns import LinkInlineProcessor
+from markdown.inlinepatterns import LinkInlineProcessor
 from markdown.inlinepatterns import ImageInlineProcessor
 # import xml.etree.ElementTree as etree
 from pelican import signals
@@ -9,6 +9,7 @@ import logging
 
 # Pattern must not consume characters
 ATTACH_IMAGE_RE = r'\!\[(?=[^\]]*?\]\(\./)'
+ATTACH_LINK_RE = r'\[(?=[^\]]*?\]\(\./)'
 
 class AttachImageInlineProcessor(ImageInlineProcessor):
     def handleMatch(self, m, data):
@@ -23,13 +24,24 @@ class AttachImageInlineProcessor(ImageInlineProcessor):
 
         return el, start, index
 
+class AttachLinkInlineProcessor(LinkInlineProcessor):
+    def handleMatch(self, m, data):
+        # Process image as usual
+        el, start, index = super().handleMatch(m, data)
+
+        # Postprocessing
+        if el is not None and el.get("href"):
+            el_oldsrc = el.get("href")
+            el.set("href", "{attach}" + el_oldsrc)
+            logging.debug(f"Coercing href '{el_oldsrc}' to '{el.get('href')}'")
+
+        return el, start, index
 
 class AutoAttachExtension(Extension):
 
     def extendMarkdown(self, md):
-        # inlinePatterns.register(ImageInlineProcessor(Image_RE, md), 'Image', 160)
-        md.inlinePatterns.register(AttachImageInlineProcessor(ATTACH_IMAGE_RE, md), 'attachImage', 150 + 1)
-        md.inlinePatterns.register(AttachImageInlineProcessor(ATTACH_IMAGE_RE, md), 'attachImage2', 150 - 1)
+        md.inlinePatterns.register(AttachImageInlineProcessor(ATTACH_IMAGE_RE, md), 'attach_image', 150 + 1)
+        md.inlinePatterns.register(AttachLinkInlineProcessor(ATTACH_LINK_RE, md), 'attach_link', 160 + 1)
 
 def pelican_init(pelican_object):
     pelican_object.settings['MARKDOWN'].setdefault('extensions', []).append(AutoAttachExtension())
