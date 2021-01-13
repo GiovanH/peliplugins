212a3931fd0466f508bd85a36b32e6edfa195b2f
Gio <sethg@ipi.org>
2021-01-12 22:59:50 -0600

    Better tweet organization

--- perma_twitter.py
+++ perma_twitter.py
@@ -7,19 +7,20 @@ import re
 import tweepy
 import xml.etree.ElementTree as ET
 import html
+import shutil
 
 from jinja2 import Environment
 # from pelican import generators
 from pelican import signals
 
 TWEETEMBED_RE = r"\[\![Tt]weet[^\]]*\]\(.+status\/(\d+).*?\)"
-TWEETLINK_RE = r"(https|http)://(www.){0,1}twitter\.com/[^/]+/status/(\d+).*?"
+TWEETLINK_RE = r"(https|http)://(www.){0,1}twitter\.com/([^/]+)/status/(\d+).*?"
         
 DEBUG = True
 
 # TODO: Clear support for *both* standalone and embeds
 # TODO: Standard retweets show RT and partial text
-# TODO: Replies don't reference the tweet they're replying to
+# TODO: Some characters (>) are double-escaped
 
 # TWEET_TEMPLATE = Template("""<p><blockquote class="twitter-tweet" data-lang="en" data-dnt="true">
 # <p lang="und" dir="ltr">{{ full_text }}</p>
@@ -32,15 +33,17 @@ DEBUG = True
 
 TWEET_TEMPLATE_STR = """<blockquote class="twitter-tweet" data-lang="en" data-dnt="true" data-nosnippet="true">
     <div class="header">
-        <a href="https://twitter.com/{{ user.screen_name }}/" title="{{ user.description|e }}">
+    {% autoescape true %}
+        <a href="https://twitter.com/{{ user.screen_name }}/" title="{{ user.description|replace("\n", " ") }}">
             <img src="{{ user.profile_image_url_https }}"
                 onerror="this.onerror=null;this.src=`https://web.archive.org/web/0/${this.src}`;"
             ></img>
             <div class="vertical">
-                <span class="name">{{ user.name|e }}</span>
+                <span class="name">{{ user.name }}</span>
                 <span class="at">@{{ user.screen_name }}</span>
             </div>
         </a>
+    {% endautoescape %}
     </div>
     <p>
         {% if in_reply_to_status_id %}
@@ -48,6 +51,7 @@ TWEET_TEMPLATE_STR = """<blockquote class="twitter-tweet" data-lang="en" data-dn
         {% endif %}
         {{ full_text|e|replace("\n", "<br></br>")|tw_stripents(id, entities, extended_entities or {}) }}
     </p>
+    <div class="media" style="display: none;">{{ full_text|e|tw_entities(id, entities, extended_entities or {}) }}</div>
     <a href="https://twitter.com/{{ user.screen_name }}/status/{{ id }}" target="_blank">{{ created_at }}</a>
 </blockquote>"""
 
@@ -88,7 +92,7 @@ def tw_entities(text, id, entities, extended_entities):
 def tw_stripents(text, id, entities, extended_entities):
     entities.update(extended_entities)
 
-    for e in entities['urls']:
+    for e in entities.get('urls', []):
         find = e['url']
         src = html.escape(e['expanded_url'])
         repl = f"<a href='{src}' target='_blank'>{e['display_url']}</a>"
@@ -135,21 +139,29 @@ class TweetEmbedProcessor(markdown.inlinepatterns.LinkInlineProcessor):
         if not handled:
             return None, None, None
 
-        href, tweet_id, title, index, handled = self.getLink(data, index)
+        href, username, tweet_id, title, index, handled = self.getLink(data, index)
         if not handled:
             return None, None, None
 
         try:
-            tweet_json = getTweetJson(tweet_id)
+            tweet_json = getTweetJson(username, tweet_id)
+
+            # Legacy support:
+            tweet_json['entities'] = tweet_json.get('entities', {})
+            tweet_json['full_text'] = tweet_json.get('full_text', tweet_json.get('text'))
+            tweet_json['extended_entities'] = tweet_json.get('extended_entities', {})
+            tweet_json['user'] = tweet_json.get('user', {})
+            tweet_json['user']['screen_name'] = tweet_json['user'].get('screen_name', 'unknown')
         except:
             logging.error("Can't load tweet " + tweet_id, exc_info=True)
             return ET.fromstring(f"<p>ERROR! Can't load tweet <a href='{href}'>'{title}'</a></p>"), m.start(0), index
 
-        string = TWEET_TEMPLATE.render(**tweet_json)
         try:
+            string = TWEET_TEMPLATE.render(**tweet_json)
             return self.md.htmlStash.store(string), m.start(0), index
         except ET.ParseError as e:
             print(string)
+            print(TWEET_TEMPLATE)
             raise e
 
     def getLink(self, data, index):
@@ -157,61 +169,63 @@ class TweetEmbedProcessor(markdown.inlinepatterns.LinkInlineProcessor):
         if handled:
             match = re.match(TWEETLINK_RE, href)
             if match:
-                http, www, tweet_id = match.groups()
-                return href, tweet_id, title, index, handled
-        return None, None, None, None, None
+                http, www, username, tweet_id = match.groups()
+                return href, username, tweet_id, title, index, handled
+        return None, None, None, None, None, None
 
-# class PelicanTweetEmbedInlineProcessor(markdown.inlinepatterns.InlineProcessor):
-#     """Inline markdown processing that matches mathjax"""
-
-#     def handleMatch(self, match, data):
-#         (tweet_id,) = match.groups()
-#         index = len(data)
-#         logging.warning(repr([match, data]))
 
 class PelicanTweetEmbedMdExtension(markdown.Extension):
-    # def extendMarkdown(self, md):
-    #     # append to end of inline patterns
-
-    #     # TWEETEMBED_RE = r"\([Tt]weet: .+status\/(\d+).*?\)"
-    #     # # TWEETEMBED_RE = r"\[\![Tt]weet\]\(.+status\/(\d+).*?\)"
-    #     # # Matches link tweets in the form of [!Tweet](https://twitter.com/gio_ebooks/status/1250287970134810624)
-    #     # tweetembedPattern = PelicanTweetEmbedInlineProcessor(TWEETEMBED_RE)
-    #     # # tweetembedPattern.md = md
-    #     # md.inlinePatterns.register(tweetembedPattern, 'tweetembed', 75)
-
-    #     # Matches link tweets in the form of [!Tweet](https://twitter.com/gio_ebooks/status/1250287970134810624)
-    #     tweetembedPattern = PelicanTweetEmbedInlineProcessor(TWEETEMBED_RE)
-    #     # tweetembedPattern.md = md
-    #     md.inlinePatterns.register(tweetembedPattern, 'tweetembed2', 200)  # Priority must exceed link
-
     def extendMarkdown(self, md):
         md.inlinePatterns.register(TweetEmbedProcessor(markdown.inlinepatterns.IMAGE_LINK_RE, md), 'tweet_embed', 200)
 
 
-def getTweetJson(tweet_id):
+def getTweetJson(username, tweet_id):
     tweet_id = str(tweet_id)
-    dest_path = os.path.join("tweets", tweet_id + ".json")
+    dest_dir = os.path.join("tweets", username)
+    dest_path = os.path.join(dest_dir, f"s{tweet_id}.json")
+
+    if not os.path.isfile(dest_path):
+        old_path = os.path.join("tweets", username[:3].lower(), username, f"s{tweet_id}.json")
+        loose_path = os.path.join("tweets", f"{tweet_id}.json")
+        if os.path.isfile(old_path):
+            logging.info("Found old-style tweet data path " + old_path)
+            os.makedirs(dest_dir, exist_ok=True)
+            shutil.move(old_path, dest_path)
+        elif os.path.isfile(loose_path):
+            logging.info("Found loose tweet data path " + loose_path)
+            os.makedirs(dest_dir, exist_ok=True)
+            shutil.move(loose_path, dest_path)
+        else:
+            pass
+            # Handled in below try
+            # raise FileNotFoundError(f"{username}, {tweet_id}")
+
     try:
         with open(dest_path, "r") as fp:
             json_obj = json.load(fp)
             logging.info("Found saved tweet data for " + tweet_id)
+
             # Temporary: Passthrough to RT
             startswith = json_obj.get("full_text", json_obj.get("text", '')).startswith("RT @")
             if startswith and (rt_obj := json_obj.get("retweeted_status")):
                 return rt_obj
-            return json_obj
+            else:
+                return json_obj
+
     except FileNotFoundError:
         # No file yet
         logging.warning("Not seeing cached tweet for id " + tweet_id)
+        # raise
 
         try:
             status = api.get_status(tweet_id, tweet_mode='extended')
             logging.info("Downloaded new tweet for id " + tweet_id)
 
+            os.makedirs(dest_dir, exist_ok=True)
             with open(dest_path, "w") as fp:
                 json.dump(status._json, fp, indent=2)
             return status._json
+
         except Exception:
             logging.error(f"Can't retrieve tweet with id '{tweet_id}'", exc_info=1)
             raise
