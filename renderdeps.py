4b4c64fa627d3fd5f68da793719042dbe145141d
Gio <sethg@ipi.org>
2023-07-23 21:27:45 -0500

    Renderdep optimizations

--- renderdeps.py
+++ renderdeps.py
@@ -8,8 +8,19 @@ from pelican.generators import ArticlesGenerator, PagesGenerator, TemplatePagesG
 import threading
 import queue
 
-MAX_THREADS = 10
-RENDERDEPS_USE_SOUP_DEFAULT = True
+MAX_THREADS = 1
+RENDERDEPS_USE_SOUP_DEFAULT = False
+
+def makeStrmatches(args, kwargs):
+    tagmatch = f"<{args[0]} " if len(args) > 0 else ""
+    classmatches = [f'class="{v}"' for v in kwargs.get('class_', [])]
+    if classmatches:
+        return [
+            f"{tagmatch}{c}"
+            for c in classmatches
+        ]
+    else:
+        return [tagmatch]
 
 def process_dependencies(article, generator=None):
     """
@@ -25,26 +36,26 @@ def process_dependencies(article, generator=None):
     settings = article.settings
     dependencies = settings.get("RENDER_DEPS", [])
     use_soup = settings.get("RENDERDEPS_USE_SOUP", RENDERDEPS_USE_SOUP_DEFAULT)
-    soup = BeautifulSoup(article._content, 'html.parser')
 
     dirty = False
 
-    for (args, kwargs), dep in dependencies:
+    for (args, kwargs, strmatches_), dep in dependencies:
         # logging.debug(f"Checking for '{args} {kwargs}'")
 
-        if match := soup.find(*args, **kwargs):
-            logging.info("Inserting dependency " + repr(dep) + " into " + repr(article.slug) + " matching" + repr(match))
-            if use_soup:
+        if use_soup:
+            soup = BeautifulSoup(article._content, 'html.parser')
+            if match := soup.find(*args, **kwargs):
+                logging.info("Inserting dependency " + repr(dep) + " into " + repr(article.slug) + " matching" + repr(match))
                 soup.append(BeautifulSoup(dep, 'html.parser'))
                 dirty = True
-            else:
+        else:
+            strmatches = strmatches_ or makeStrmatches(args, kwargs)
+            if any(s in article._content for s in strmatches):
+                logging.debug(f"Matched '{strmatches}'")
                 article._content += dep
                 # just chuck it in
-        else:
-            # logging.debug("Not found")
-            pass
 
-    if dirty:
+    if use_soup and dirty:
         article._content = str(soup)
     else:
         # logging.debug("No dependencies in " + repr(article.slug))
@@ -69,21 +80,24 @@ def link_source_files(generator):
 def add_deps(generators):
     # Process the articles and pages
 
-    q = queue.Queue()
+    THREADED = MAX_THREADS > 1
 
-    class Worker(threading.Thread):
-        def __init__(self, fn, *args, **kwargs):
-            self.fn = fn
-            super().__init__(*args, **kwargs)
+    if THREADED:
+        q = queue.Queue()
 
-        def run(self):
-            while True:
-                try:
-                    work = q.get(timeout=3)  # 3s timeout
-                except queue.Empty:
-                    return
-                self.fn(*work)
-                q.task_done()
+        class Worker(threading.Thread):
+            def __init__(self, fn, *args, **kwargs):
+                self.fn = fn
+                super().__init__(*args, **kwargs)
+
+            def run(self):
+                while True:
+                    try:
+                        work = q.get(timeout=3)  # 3s timeout
+                    except queue.Empty:
+                        return
+                    self.fn(*work)
+                    q.task_done()
 
     document_generators = [ArticlesGenerator, PagesGenerator, TemplatePagesGenerator]
 
@@ -95,14 +109,20 @@ def add_deps(generators):
                 if getattr(generator, attr, None)
             ], [])
             for document in documents:
-                work = (document, generator)
-                q.put_nowait(work)
+                if THREADED:
+                    work = (document, generator)
+                    q.put_nowait(work)
+                else:
+                    process_dependencies(document, generator)
         else:
             logging.debug(f"Renderdeps: Unhandled generator {generator}")
 
-    for _ in range(MAX_THREADS):
-        Worker(process_dependencies).start()
-    q.join()  # blocks until the queue is empty.
+    if THREADED:
+        for _ in range(MAX_THREADS):
+            Worker(process_dependencies).start()
+        q.join()  # blocks until the queue is empty.
+
+
 
 def register():
     signals.all_generators_finalized.connect(add_deps)
